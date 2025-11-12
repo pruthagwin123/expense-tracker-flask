403e5a00c6153ae417a65d6384a2c1e6b70e846e
PRUTHAGWIN CHALLURI <pruthagwinch@gmail.com>
2025-11-12 20:35:04 +0530

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -298,4 +298,7 @@ def chart_data():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
